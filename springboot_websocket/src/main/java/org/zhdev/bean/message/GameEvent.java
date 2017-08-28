b2d7a298f8f0a8b6848787912a7f66e7f01eb84a
zh <zh-dev@outlook.com>
2017-08-28 17:29:23 +0800

    Merge branch 'master' of https://github.com/Jikerr/dev
    # Conflicts:
    #	springboot_websocket/pom.xml
    #	springboot_websocket/src/main/java/org/zhdev/bean/message/ResponseMessage.java
    #	springboot_websocket/src/main/java/org/zhdev/config/WebMvcConfig.java
    #	springboot_websocket/src/main/java/org/zhdev/config/WebSocketConfig.java
    #	springboot_websocket/src/main/java/org/zhdev/controller/WsController.java
    #	springboot_websocket/src/main/resources/static/js/main.js
    #	springboot_websocket/src/main/resources/templates/index.html

---
+++ springboot_websocket/src/main/java/org/zhdev/bean/message/GameEvent.java
@@ -0,0 +1,13 @@
+package org.zhdev.bean.message;
+
+/**
+ * Created by MACHENIKE on 2017/8/28.
+ */
+public class GameEvent {
+    public final static String GAME_STATE_MENU = "GAME_STATE_MENU";
+    public final static String GAME_STATE_INIT = "GAME_STATE_INIT";
+    public final static String GAME_STATE_START = "GAME_STATE_START";
+    public final static String GAME_STATE_WIN = "GAME_STATE_WIN";
+    public final static String GAME_STATE_OVER = "GAME_STATE_OVER";
+
+}
---
+++ springboot_websocket/src/main/java/org/zhdev/bean/message/UserEvent.java
@@ -0,0 +1,9 @@
+package org.zhdev.bean.message;
+
+/**
+ * Created by MACHENIKE on 2017/8/28.
+ */
+public class UserEvent {
+    //public final static String
+
+}
